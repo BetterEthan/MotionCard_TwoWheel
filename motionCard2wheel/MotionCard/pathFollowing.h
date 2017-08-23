4961ae90eef67030fb2bb379cf301efa239d67e7
BetterEthan <xupengneu@stumail.neu.edu.cn>
2017-08-23 10:14:08 +0800

    控制卡两轮，没有速度规划，点到点函数为外部实现

---
+++ motionCard2wheel/MotionCard/pathFollowing.h
@@ -0,0 +1,16 @@
+#ifndef _PATHFOLLOWING_H
+#define _PATHFOLLOWING_H
+
+
+
+
+
+
+
+#include "calculate.h"
+
+
+int PathFollowing(float percent,float vell);
+float AngleControl(float anglePresent,float angleTarget);
+
+#endif
